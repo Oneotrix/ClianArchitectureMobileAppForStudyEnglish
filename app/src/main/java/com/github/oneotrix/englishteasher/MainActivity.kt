dd2a986783bbf075d00f5840cf510ce2704ae88f
oneotrix <m5hutov@gmail.com>
2023-05-12 23:40:26 +0300

    add SplashScreenFragment

--- app/src/main/java/com/github/oneotrix/englishteasher/MainActivity.kt
+++ app/src/main/java/com/github/oneotrix/englishteasher/MainActivity.kt
@@ -2,10 +2,25 @@ package com.github.oneotrix.englishteasher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.oneotrix.englishteasher.databinding.ActivityMainBinding
+import com.github.oneotrix.englishteasher.presentation.SplashScreenFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            val fragment = SplashScreenFragment.newInstance()
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(binding.fragmentContainer.id, fragment)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/github/oneotrix/englishteasher/presentation/SplashScreenFragment.kt
@@ -0,0 +1,29 @@
+package com.github.oneotrix.englishteasher.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.oneotrix.englishteasher.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment: Fragment() {
+
+    private lateinit var binding: FragmentSplashScreenBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+    companion object {
+        fun newInstance() = SplashScreenFragment()
+    }
+
+}
