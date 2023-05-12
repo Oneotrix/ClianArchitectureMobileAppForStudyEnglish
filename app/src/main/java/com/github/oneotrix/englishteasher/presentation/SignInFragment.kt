4b1b006007573239edfc25d8c117441ddf286107
oneotrix <m5hutov@gmail.com>
2023-05-13 00:06:43 +0300

    add SignIn Fragment

---
+++ app/src/main/java/com/github/oneotrix/englishteasher/presentation/SignInFragment.kt
@@ -0,0 +1,27 @@
+package com.github.oneotrix.englishteasher.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.oneotrix.englishteasher.databinding.FragmentSignInBinding
+
+class SignInFragment: Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+
+        return  binding.root
+    }
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+}
